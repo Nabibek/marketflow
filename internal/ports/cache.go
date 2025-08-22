5414f1f7e6ea0069e9b2999f5cc67fa74b848d15
Nabibek <nabibiek@mail.ru>
2025-08-22 05:12:41 +0500

    upd: rename func

--- internal/ports/cache.go
+++ internal/ports/cache.go
@@ -2,14 +2,13 @@ package ports
 
 import (
 	"context"
-	"time"
-
 	"marketflow/internal/domain"
+	"time"
 )
 
 type Cache interface {
 	PushTick(cxt context.Context, t domain.PriceTick) error
-	GetLastest(cxt context.Context, symbol string) (map[string]float64, error)
+	GetLatest(cxt context.Context, symbol string) (map[string]float64, error)
 	GetWindow(cxt context.Context, exchange, symbol string, d time.Duration) ([]domain.PriceTick, error)
 	TrimOld(cxt context.Context, olderThan time.Time) error
 }
